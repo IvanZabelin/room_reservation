09257c3edc26ddb348e1be35ccf4b68378d2898a
Ivan Zabelin <the.zabelin@gmail.com>
2025-04-03 10:46:10 +0300

    add model

--- app/core/config.py
+++ app/core/config.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     app_descriptian: str = 'Сервис для бронирования переговорных комнат.'
+    database_url: str
 
     class Config:
         env_file = '.env'
