02de22a339d150e4737fe99279850fd27a451290
Ivan Zabelin <the.zabelin@gmail.com>
2025-04-15 15:59:33 +0300

    update

--- app/schemas/reservation.py
+++ app/schemas/reservation.py
@@ -1,10 +1,22 @@
-from datetime import datetime
-from pydantic import BaseModel, root_validator, validator
+from datetime import datetime, timedelta
+from pydantic import BaseModel, Extra, Field, root_validator, validator
+
+
+FROM_TIME = (
+    datetime.now() + timedelta(minutes=10)
+).isoformat(timespec='minutes')
+
+TO_TIME = (
+    datetime.now() + timedelta(hours=1)
+).isoformat(timespec='minutes')
 
 
 class ReservationBase(BaseModel):
-    from_reserve: datetime
-    to_reserve: datetime
+    from_reserve: datetime = Field(..., example=FROM_TIME)
+    to_reserve: datetime = Field(..., example=TO_TIME)
+
+    class Config:
+        extra = Extra.forbid
 
     @root_validator
     def check_from_reserve_before_to_reserve(cls, values):
