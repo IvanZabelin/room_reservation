cf1c1621d0c5673d99040e4d1a37c8e7aacd9634
Ivan Zabelin <the.zabelin@gmail.com>
2025-04-17 10:04:00 +0300

    Add user

--- app/core/config.py
+++ app/core/config.py
@@ -5,6 +5,7 @@ class Settings(BaseSettings):
     app_title: str = 'Бронирование переговорок'
     app_descriptian: str = 'Сервис для бронирования переговорных комнат.'
     database_url: str
+    secret: str = 'SECRET'
 
     class Config:
         env_file = '.env'
